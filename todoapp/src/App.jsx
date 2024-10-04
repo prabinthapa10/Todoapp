9ce0501cec5d9495a45a2830acc14a1cf6753fa1
prabinthapa10 <prabinmagar636@gmail.com>
2024-10-04 10:38:50 +0545

    adding values to todos

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -2,9 +2,30 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // static variable which store value and array  setTodos to store many values
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  return <>Hello</>;
+  // handle submit
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <div>
+      <div>Todolist Apps</div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+          />
+          <p>{todo}</p>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
